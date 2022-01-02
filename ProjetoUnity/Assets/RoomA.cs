167b1c946cb803f984bf04ae44a25602f32a8f8a
Miguel S Silva <miguel.s.silva@ua.pt>
2022-01-02 16:40:01 +0000

    Color perception

--- ProjetoUnity/Assets/RoomA.cs
+++ ProjetoUnity/Assets/RoomA.cs
@@ -9,10 +9,14 @@ public class RoomA : MonoBehaviour
     public Transform thatcherRotatedPos;
     private GameObject thatcherObj;
 
+    public GameObject colorPerceptionPrefab;
+    private GameObject colorPerceptionObj;
+
     private string currentDisplay;
 
     private GameObject objToLerp;
     private Quaternion lerpDestination;
+    public Light roomLight;
     void Start()
     {
         
@@ -20,20 +24,38 @@ public class RoomA : MonoBehaviour
 
     private void ThatcherEffect()
     {
+        roomLight.intensity = 7.5f;
         if (currentDisplay == "thatcher") {
             objToLerp = thatcherObj;
             lerpDestination = thatcherRotatedPos.rotation;
         } else
         {
+            Destroy(colorPerceptionObj);
             thatcherObj = Instantiate(thatcherPrefab, thatcherPos.position,thatcherPos.rotation);
         }
         currentDisplay = "thatcher";
+    }
+
+    private void ColorPerception()
+    {
+        Destroy(thatcherObj);
+        if (currentDisplay != "ColorPerception")
+        {
+            roomLight.intensity = 1f;
+            currentDisplay = "ColorPerception";
+        } else
+        {
+            colorPerceptionObj = Instantiate(colorPerceptionPrefab);
+        }
     }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             ThatcherEffect();
+        } else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ColorPerception();
         }
 
         if (objToLerp != null)
