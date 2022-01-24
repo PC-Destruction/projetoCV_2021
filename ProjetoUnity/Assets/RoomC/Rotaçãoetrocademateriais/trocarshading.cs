6acbb69ea27fbc3a45de05b38335d0b7d53de3f6
Coanhas <94471561+Coanhas@users.noreply.github.com>
2022-01-24 16:53:41 +0000

    tips

--- ProjetoUnity/Assets/RoomC/Rotaçãoetrocademateriais/trocarshading.cs
+++ ProjetoUnity/Assets/RoomC/Rotaçãoetrocademateriais/trocarshading.cs
@@ -21,14 +21,17 @@ public class trocarshading : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1)) { 
 
             GetComponent<MeshRenderer>().material = flat;
+            GameController.instance.SetToolTipText("The lighting is evaluated only once for each polygon.");
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             GetComponent<MeshRenderer>().material = gouraud;
+            GameController.instance.SetToolTipText("The illumination model to each vertex is calculated the light intensity from the vertex normal.");
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             GetComponent<MeshRenderer>().material = phong;
+            GameController.instance.SetToolTipText("The light intensities the normals are interpolated between the vertices.");
         }
 
     }
